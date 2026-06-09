aaa1dfdb30db244993f1ebdd92f13cf7c8b7cb9c
Nihal Mishra <nihalmishra3009@gmail.com>
2026-06-09 18:06:55 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-window.__API_BASE = "https://obe-course-attainment-system-production-aced.up.railway.app";
+window.__API_BASE = "https://obe-course-attainment-system-production-4800.up.railway.app";
